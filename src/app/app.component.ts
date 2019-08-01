ca59e36732dbcfbb318d237593dd7c0d0a8368b7
briansain <brian.sain@yahoo.com>
2019-07-31 22:58:23 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { share, map, tap } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'ft-root',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-modules';
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      tap(result => console.log(result)),
+      share()
+    );
+  testing = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.isHandset$.subscribe(res => {
+      this.testing = res;
+      console.log(this.testing);
+    });
+  }
+
 }
---
+++ src/app/meal-planning/meal-planning.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MealPlanningRoutingModule } from './meal-planning-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MealPlanningRoutingModule
+  ]
+})
+export class MealPlanningModule { }
